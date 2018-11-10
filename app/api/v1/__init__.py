c168b792fc00e7d99d0e7f303fe6902a09973e9c
MandelaK <kazi.keith@gmail.com>
2018-11-11 00:59:00 +0300

    [Starts #161709665] Registered views that handle requests for canceling orders

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import GenericParcel, SpecificParcel
+from .views import GenericParcel, SpecificParcel, Cancel
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 api.add_resource(GenericParcel, '/parcels')
 api.add_resource(SpecificParcel, '/parcels/<int:id>')
+api.add_resource(Cancel, '/parcels/<int:id>/cancel')
