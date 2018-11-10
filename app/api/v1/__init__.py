93fa834af2d9d28ac1f32764bf1a07635565d9e5
MandelaK <kazi.keith@gmail.com>
2018-11-11 01:34:04 +0300

    [Starts #161710587]Registered views that allow the user to change destination

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import GenericParcel, SpecificParcel, Cancel
+from .views import GenericParcel, SpecificParcel, Cancel, Admin, Destination
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 api.add_resource(GenericParcel, '/parcels')
 api.add_resource(SpecificParcel, '/parcels/<int:id>')
+api.add_resource(Admin, '/admin/location/<int:id>')
 api.add_resource(Cancel, '/parcels/<int:id>/cancel')
+api.add_resource(Destination, '/parcels/<int:id>/destination')
