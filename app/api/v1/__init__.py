5a5dc451bced1d0cc18712f3f1b40d66f934a4a8
MandelaK <kazi.keith@gmail.com>
2018-11-11 02:07:48 +0300

    [Starts #161709558] Registered the views for enabling us to get all deliveries by one user

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,12 +1,12 @@
+from .views import GenericParcel, User, SpecificParcel, Admin, Cancel, Destination
 from flask import Blueprint
 from flask_restful import Api
-
-from .views import GenericParcel, SpecificParcel, Cancel, Admin, Destination
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 api.add_resource(GenericParcel, '/parcels')
+api.add_resource(User, '/users/<int:user_id>/parcels')
 api.add_resource(SpecificParcel, '/parcels/<int:id>')
 api.add_resource(Admin, '/admin/location/<int:id>')
 api.add_resource(Cancel, '/parcels/<int:id>/cancel')
