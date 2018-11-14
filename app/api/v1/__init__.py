6005146ec43598161fe25310b5473c168b84bcf9
MandelaK <kazi.keith@gmail.com>
2018-11-14 06:29:42 +0300

    chore(feedback): update v1/__init__.py file so that correct parcel_id is passed
    - update __init__.py file to resolve issues with testing the code. id was being passed instead of parcel_id hence making all tests fail that required parcel_id
    
    [closes #161932219]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,12 +2,12 @@ from .views import GenericParcel, User, SpecificParcel, Admin, Cancel, Destinati
 from flask import Blueprint
 from flask_restful import Api
 
-version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
+version1 = Blueprint("v1", __name__, url_prefix="/api/v1")
 api = Api(version1)
 
-api.add_resource(GenericParcel, '/parcels')
-api.add_resource(User, '/users/<int:user_id>/parcels')
-api.add_resource(SpecificParcel, '/parcels/<int:id>')
-api.add_resource(Admin, '/admin/location/<int:id>')
-api.add_resource(Cancel, '/parcels/<int:id>/cancel')
-api.add_resource(Destination, '/parcels/<int:id>/destination')
+api.add_resource(GenericParcel, "/parcels")
+api.add_resource(User, "/users/<int:user_id>/parcels")
+api.add_resource(SpecificParcel, "/parcels/<int:parcel_id>")
+api.add_resource(Admin, "/admin/location/<int:parcel_id>")
+api.add_resource(Cancel, "/parcels/<int:parcel_id>/cancel")
+api.add_resource(Destination, "/parcels/<int:parcel_id>/destination")
