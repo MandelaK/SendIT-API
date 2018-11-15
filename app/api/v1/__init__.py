ea23d26ae6130ee08e1e2fb1f138d72066b1d8e5
MandelaK <kazi.keith@gmail.com>
2018-11-15 18:47:24 +0300

    chore(error-handling): add error handler for 404 pages
    add and implement builtin Flask error handler so as to returning more informative error messages and ensure non-existent links don't crash the application
    
    [starts #161982249]
    [finishes #161982249]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
-from .views import GenericParcel, User, SpecificParcel, Admin, Cancel, Destination
+from .views import (GenericParcel, User, SpecificParcel,
+                    Admin, Cancel, Destination)
 from flask import Blueprint
 from flask_restful import Api
 
 version1 = Blueprint("v1", __name__, url_prefix="/api/v1")
-api = Api(version1)
+api = Api(version1, catch_all_404s=True)
 
 api.add_resource(GenericParcel, "/parcels")
 api.add_resource(User, "/users/<int:user_id>/parcels")
